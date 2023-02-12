ee110c98f37df6d627d238f1e0e3ca78142e9a72
Daniela Carballo <danscarballo@gmail.com>
2023-02-12 19:54:36 -0300

    Se agrega la funcion secuencia y mostrarSecuencia

--- script.js
+++ script.js
@@ -4,32 +4,40 @@ const $rojo = document.querySelector('#color-rojo');
 const $amarillo = document.querySelector('#color-amarillo');
 const $azul = document.querySelector('#color-azul');
 const $nivel = document.querySelector('#nivel');
+const colores = [$verde, $rojo, $amarillo, $azul];
 
 
 $botonIniciar.addEventListener('click', () => {
-    console.log(Secuencia());
+    secuencia()
 });
-
-const Colores = [$verde, $rojo, $amarillo, $azul];
 
 let secuenciaHumano = [];
 let secuenciaMaquina = [];
 let nivel = 0;
+let secuenciaHumanoCompleta = true;
 
-const numeroAleatorio = function (){
-    const $numeroAleatorio = Math.floor(Math.random() * (Colores.length));
-    return $numeroAleatorio;
+const secuencia = function (){
+    const $numeroAleatorio = Math.floor(Math.random() * (colores.length));
+
+    secuenciaMaquina.push($numeroAleatorio);
+    nivel++;
+    $nivel.innerHTML = `Nivel: ${nivel}`;
+
+    console.log(secuenciaMaquina);
+    mostrarSecuencia();
+    copiarSecuencia()
+    compararSecuencia()
 };
 
-const Secuencia = function (){
-    secuenciaMaquina.push(numeroAleatorio());
-    return secuenciaMaquina;
+const mostrarSecuencia = function (){
+    for (let i = 0; i < secuenciaMaquina.length; i++){
+        const color = colores[secuenciaMaquina[i]];
+        setTimeout(() => {
+            color.classList.add('flash');
+            setTimeout(() => {
+                color.classList.remove('flash');
+            }, 500);
+        }, 1200 * (i + 1));
+    }
 };
-
-
-
-
-
-
-
 
